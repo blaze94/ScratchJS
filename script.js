0c99845f4517fd1be7b2ad0d562b28a6f28c65cc
sean Kim <30333292+nabithecat@users.noreply.github.com>
2023-08-26 22:35:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const scContainer = document.getElementById('js--sc--container');
+
+const sc = new ScratchCard('js--sc--container', {
+    scratchType: SCRATCH_TYPE.LINE,
+    containerWidth: scContainer.offsetWidth,
+    containerHeight: 300,
+    imageForwardSrc: '/peterCrown.png',
+    imageBackgroundSrc: 'ScratchCover.png',
+    clearZoneRadius: 30,
+    nPoints: 0,
+    pointSize: 0,
+})
+
+sc.init().then(() => {
+    sc.canvas.addEventListener('scratch.move', () => {
+        let percent = sc.getPercent().toFixed(0);
+        scInfos.innerHTML = percent + '%';
+        console.log(percent)
+    })
+}).catch((error) => {
+    alert(error.message);
+});
